09755a6b5b3b753403b9031d72b5639328174808
Taeyang <1316237481@qq.com>
2020-01-06 22:57:38 +0800

    1.6-ev-gw

--- components/detail/detail.js
+++ components/detail/detail.js
@@ -12,6 +12,15 @@ Component({
       wx.setNavigationBarTitle({
         title: '演出详情'
       })
+
+      wx.request({
+        url: 'https://api.piaoniu.com/v1/activityPageDetail?activityId=130267&mweb=false&supportSpeedPackBuy=true',
+        success:  (res) => {
+          this.setData({
+            detailData: res.data
+          })
+        }
+      })
     }
   },
 
@@ -19,7 +28,7 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    detailData: {}
   },
 
   /**
